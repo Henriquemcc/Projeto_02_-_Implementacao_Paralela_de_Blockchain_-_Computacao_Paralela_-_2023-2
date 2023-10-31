0e85459e4842734e2866b281a74bfcb50fa0f997
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-10-31 11:02:44 -0300

    Adicionando script para testar as versões do programa

---
+++ Testar.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Limpando
+bash ./Limpar.bash
+
+# Compilando
+bash ./Compilar.bash
+
+# Comparando as saídas
+bash ./CompararSaidas.bash
+
+# Cronometrando o tempo gasto
+bash ./Cronometrar.bash
+
