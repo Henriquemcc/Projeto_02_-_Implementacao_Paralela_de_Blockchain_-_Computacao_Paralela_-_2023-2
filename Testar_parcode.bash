4e13e36cd40b92e896c263e3957c530e0d5dc6af
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-10-31 11:02:51 -0300

    Adicionando script para testar as versões do programa no parcode

---
+++ Testar_parcode.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Limpando
+bash ./Limpar.bash
+
+# Compilando
+bash ./Compilar_parcode.bash
+
+# Comparando as saídas
+bash ./CompararSaidas.bash
+
+# Cronometrando o tempo gasto
+bash ./Cronometrar.bash
+
