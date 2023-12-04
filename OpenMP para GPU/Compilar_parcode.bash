7498976973096292a7ad0cf1f8e5ce50bae73921
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-12-04 17:28:17 -0300

    Adicionando a compilação do teste da função sha256

--- OpenMP para GPU/Compilar_parcode.bash
+++ OpenMP para GPU/Compilar_parcode.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 gcc8 -lstdc++ -o TestChain -std=c++11 -x c++ main.cpp Block.cpp Blockchain.cpp sha256.cpp -fopenmp
+gcc8 -lstdc++ -o TestSha256 -std=c++11 -x c++ TestSha256.cpp sha256.cpp -fopenmp
