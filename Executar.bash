4855394f72f459c997b3203e35bf3f28f159fb54
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-12-04 18:04:35 -0300

    Adicionando print da versão que está sendo executada

--- Executar.bash
+++ Executar.bash
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 # Executando versão sequencial
+echo "Versão sequencial"
 cd Sequencial || exit
 ./TestChain
 cd .. || exit
 
 # Executando versão OpenMP para multicore
+echo "Versão OpenMP para multicore"
 cd 'OpenMP para multicore' || exit
 ./TestChain
 cd .. || exit
 
 # Executando versão OpenMP para GPU
+echo "Versão OpenMP para GPU"
 cd 'OpenMP para GPU' || exit
 ./TestChain
 cd .. || exit
 
 # Executando versão CUDA para GPU
+echo "Versão CUDA para GPU"
 cd 'CUDA para GPU' || exit
 ./TestChain
 cd .. || exit
