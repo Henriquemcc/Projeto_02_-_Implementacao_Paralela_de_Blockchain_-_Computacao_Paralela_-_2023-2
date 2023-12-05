f125c202266fadf7167f2d7dc5f483b30d67be6d
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-12-05 10:39:53 -0300

    Alterando script de compilação para compilar arquivo .cu

--- CUDA para GPU/Compilar.bash
+++ CUDA para GPU/Compilar.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nvcc main.cpp Block.cpp Blockchain.cpp sha256.cpp -o TestChain
-nvcc TestSha256.cpp sha256.cpp -o TestSha256
+nvcc -allow-unsupported-compiler main.cpp Block.cpp Blockchain.cpp sha256.cu -o TestChain
+nvcc -allow-unsupported-compiler TestSha256.cpp sha256.cu -o TestSha256
