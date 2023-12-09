f84c4e925d1c55d3755ebf03ec65b15c5101aab3
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-12-09 15:10:57 -0300

    Refatorando o código

--- OpenMP para GPU/Compilar_parcode.bash
+++ OpenMP para GPU/Compilar_parcode.bash
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-cat sha256.h > TestChain.cpp
-cat sha256.cpp >> TestChain.cpp
-cat Block.h >> TestChain.cpp
-cat Block.cpp >> TestChain.cpp
-cat Blockchain.h >> TestChain.cpp
-cat Blockchain.cpp >> TestChain.cpp
-cat main.cpp >> TestChain.cpp
+{
+  cat sha256.h
+  cat sha256.cpp
+  cat Block.h
+  cat Block.cpp
+  cat Blockchain.h
+  cat Blockchain.cpp
+  cat main.cpp
+} > TestChain.cpp
+
 gcc8 -lstdc++ -o TestChain -std=c++11 -x c++ TestChain.cpp
 
-cat sha256.h > TestSha256_.cpp
-cat sha256.cpp >> TestSha256_.cpp
-cat TestSha256.cpp >> TestSha256_.cpp
+{
+  cat sha256.h
+  cat sha256.cpp
+  cat TestSha256.cpp
+} > TestSha256_.cpp
+
 gcc8 -lstdc++ -o TestSha256 -std=c++11 -x c++ TestSha256_.cpp
