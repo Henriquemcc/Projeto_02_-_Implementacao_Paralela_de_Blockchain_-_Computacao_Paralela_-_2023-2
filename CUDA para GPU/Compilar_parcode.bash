0f262604accab084bf632946cde1e2950adfbcbe
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-12-09 14:56:42 -0300

    Corrigindo erro de compilação no Parcode

--- CUDA para GPU/Compilar_parcode.bash
+++ CUDA para GPU/Compilar_parcode.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-gcc_path=$(whereis gcc8 | cut -d' ' -f2)
-nvcc -allow-unsupported-compiler --compiler-bindir="$gcc_path" main.cpp Block.cpp Blockchain.cpp sha256.cu -o TestChain
-nvcc -allow-unsupported-compiler --compiler-bindir="$gcc_path" TestSha256.cpp sha256.cu -o TestSha256
+nvcc -std=c++11 main.cpp Block.cpp Blockchain.cpp sha256.cu -o TestChain
+nvcc -std=c++11 TestSha256.cpp sha256.cu -o TestSha256
