e41f21b18b691d412cc63e413b1a4d215ceb1804
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2023-12-11 22:13:26 -0300

    Repetindo o processo de adição de bloco 20 vezes

--- OpenMP para multicore/main.cpp
+++ OpenMP para multicore/main.cpp
@@ -4,14 +4,11 @@ int main()
 {
     Blockchain bChain = Blockchain();
 
-    cout << "Mining block 1..." << endl;
-    bChain.AddBlock(Block(1, "Block 1 Data"));
-
-    cout << "Mining block 2..." << endl;
-    bChain.AddBlock(Block(2, "Block 2 Data"));
-
-    cout << "Mining block 3..." << endl;
-    bChain.AddBlock(Block(3, "Block 3 Data"));
+    for (int i=0;i<20;i++) {
+        cout << "Mining block "<< i << "..." << endl;
+        std::string mensagem = "Block " + std::to_string(i) + " Data";
+        bChain.AddBlock(Block(i, mensagem));
+    }
 
     return 0;
 }
